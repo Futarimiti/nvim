55af2d805e5418fb1e090c2049570fecf2cf8a38
Futar <Futarimiti@163.com>
2024-07-26 16:58:33 +0800

    Revert "fix(ftplugin/haskell): fix include option"
    This reverts commit 6717f7b3ba96d19ddb780473e0eeb0a393059e17.

--- after/ftplugin/haskell/include.lua
+++ after/ftplugin/haskell/include.lua
@@ -1,5 +1,6 @@
 vim.opt_local.includeexpr = [[substitute(v:fname,'\.','/','g')]]
 vim.opt_local.suffixesadd = { '.hs', '.lhs' }
-vim.opt_local.include =
-  [[\v^\s*import\s*(safe\s*)?(qualified\s*)?("\zs[^"]+\ze")?\s*\zs[0-9A-Za-z\._]+]]
+-- FIXME: `import "package-name"Path.To.Module` gives `"Path.To.Module`
+-- however should suffice most cases with proper spacing
+vim.opt_local.include = [[\v^import(.*"|\s+(safe\s+)?(qualified)?)]]
 vim.opt_local.define = [[^\(data\s*\|type\s*\|newtype\s*\|\s*\ze\i\+\s*\(::\|.*\s=\|<-\)\)]]
