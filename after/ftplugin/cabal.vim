7da14d4c824683d0826d6c26be85f76e0a955d16
Futar <Futarimiti@163.com>
2024-07-28 11:19:22 +0800

    perf haskell support

--- after/ftplugin/cabal.vim
+++ after/ftplugin/cabal.vim
@@ -1,7 +1,7 @@
 setlocal shiftwidth=2
 setlocal tabstop=2
 setlocal expandtab
-setlocal comments=s1fl:{-,mb:-,ex:-},:--
+setlocal comments=:--
 setlocal commentstring=--\ %s
 
 compiler cabal
--- after/ftplugin/haskell/include.lua
+++ after/ftplugin/haskell/include.lua
@@ -2,5 +2,6 @@ vim.opt_local.includeexpr = [[substitute(v:fname,'\.','/','g')]]
 vim.opt_local.suffixesadd = { '.hs', '.lhs' }
 -- FIXME: `import "package-name"Path.To.Module` gives `"Path.To.Module`
 -- however should suffice most cases with proper spacing
+-- Maybe check out src/search.c:3495 for reasons
 vim.opt_local.include = [[\v^import(.*"|\s+(safe\s+)?(qualified)?)]]
 vim.opt_local.define = [[^\(data\s*\|type\s*\|newtype\s*\|\s*\ze\i\+\s*\(::\|.*\s=\|<-\)\)]]
