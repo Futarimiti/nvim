e2d2b7204d5355a27a26e1a5339dba34a02e9d8d
Futar <Futarimiti@163.com>
2024-07-30 15:42:57 +0800

    wip: add aerial, brief configure

---
+++ plugin/navigation/aerial.lua
@@ -0,0 +1,11 @@
+require('aerial').setup {
+  -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+  on_attach = function(bufnr)
+    -- Jump forwards/backwards with '{' and '}'
+    -- vim.keymap.set('n', '{', '<CMD>AerialPrev<CR>', { buffer = bufnr })
+    -- vim.keymap.set('n', '}', '<CMD>AerialNext<CR>', { buffer = bufnr })
+  end,
+}
+
+-- You probably also want to set a keymap to toggle aerial
+vim.keymap.set('n', '<localleader>a', '<CMD>AerialToggle<CR>')
