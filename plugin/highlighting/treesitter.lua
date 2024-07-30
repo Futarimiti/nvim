591afe8617d0ea2286b1d23cd153ed91927108a1
Futar <Futarimiti@163.com>
2024-07-30 23:55:53 +0800

    wip: add keymaps for incremental selection

--- plugin/highlighting/treesitter.lua
+++ plugin/highlighting/treesitter.lua
@@ -7,6 +7,12 @@ require('nvim-treesitter.configs').setup {
   },
   incremental_selection = {
     enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
+    },
   },
   indent = {
     enable = true,
