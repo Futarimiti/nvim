88713b4b2756066974e452dd90eb88d71b4c31ea
Futar <Futarimiti@163.com>
2024-08-10 08:16:05 +0100

    set up `:Lex`-like behaviour in oil
    phew!

--- plugin/browsing/explorer/oil.lua
+++ plugin/browsing/explorer/oil.lua
@@ -1,4 +1,26 @@
 local oil = require 'oil'
+
+local open = function(path)
+  local win = vim.w.open_from_win or 0
+  vim.api.nvim_win_call(win, function() vim.cmd.edit(path) end)
+  vim.api.nvim_set_current_win(win)
+end
+
+local oil_toggle = function(path)
+  local oil_windows = vim
+    .iter(vim.fn.getwininfo())
+    :filter(function(win) return vim.bo[win.bufnr].filetype == 'oil' end)
+
+  if oil_windows:peek() then
+    oil_windows:each(function(win) pcall(vim.api.nvim_win_close, win.winid, true) end)
+  else
+    local open_from = vim.api.nvim_get_current_win()
+    vim.cmd 'topleft 35vsplit'
+    oil.open(path)
+    vim.w.open_from_win = open_from
+  end
+end
+
 oil.setup {
   delete_to_trash = true,
   skip_confirm_for_simple_edits = true,
@@ -15,20 +37,33 @@ oil.setup {
         :any(function(suffix) return vim.endswith(name, suffix) end)
     end,
   },
+  keymaps = {
+    ['<CR>'] = {
+      mode = 'n',
+      desc = 'Open the entry under the cursor in the window that starts oil',
+      callback = function()
+        local entry = oil.get_cursor_entry()
+        if entry.type == 'directory' then
+          require('oil.actions').select.callback()
+        elseif entry.type == 'link' then
+          local linkto = entry.meta.link
+          local path = vim.fs.normalize(linkto)
+          if vim.fn.isdirectory(path) then
+            require('oil.actions').select.callback()
+          else
+            open(path)
+          end
+        else
+          -- .name or .parsed_name?
+          local tail = entry.name
+          local cwd, _ = vim.api.nvim_buf_get_name(0):gsub('^oil://', '')
+          local path = vim.fs.joinpath(cwd, tail)
+          open(path)
+        end
+      end,
+    },
+  },
 }
-
-local oil_toggle = function(path)
-  local oil_windows = vim
-    .iter(vim.fn.getwininfo())
-    :filter(function(win) return vim.bo[win.bufnr].filetype == 'oil' end)
-
-  if oil_windows:peek() then
-    oil_windows:each(function(win) pcall(vim.api.nvim_win_close, win.winid, true) end)
-  else
-    vim.cmd 'topleft 40vsplit'
-    oil.open(path)
-  end
-end
 
 -- relative to file
 vim.keymap.set('n', '<localleader>v', function() oil_toggle(vim.fn.expand '%:p:h') end)
