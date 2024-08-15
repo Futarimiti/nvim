2d0f7314799b776b8d2fbd8967b7f4bef4dc702b
Futar <Futarimiti@163.com>
2024-08-15 23:15:00 +0100

    configure quicker keymaps and highlight

--- plugin/quickfix/quicker.lua
+++ plugin/quickfix/quicker.lua
@@ -3,5 +3,19 @@ local quicker = require 'quicker'
 quicker.setup {
   borders = {
     vert = ' ',
+    strong_cross = '━',
+    soft_cross = '╌',
+  },
+  keys = {
+    {
+      'zR',
+      quicker.expand,
+      desc = 'Expand quickfix context',
+    },
+    {
+      'zM',
+      quicker.collapse,
+      desc = 'Collapse quickfix context',
+    },
   },
 }
