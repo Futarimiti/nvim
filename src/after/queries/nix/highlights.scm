0c4e25529e90b8753768f0c66d7bbcafbe9461ed
Carman Fu <huivioemp1bs@posteo.com>
2026-05-30 16:50:31 +0800

    fix(queries/nix): highlight all uri in strings

--- src/after/queries/nix/highlights.scm
+++ src/after/queries/nix/highlights.scm
@@ -1,30 +1,8 @@
 ;; extends
 
-;; highlight flake input url
-(source_code
-  expression: (attrset_expression
-    (binding_set
-      binding: (binding
-        attrpath: (attrpath
-          attr: (identifier) @_inputs
-          (#eq? @_inputs "inputs"))
-        expression: (attrset_expression
-          (binding_set
-            binding: [
-              (binding
-                attrpath: (attrpath
-                  attr: (identifier)
-                  attr: (identifier) @_url
-                  (#eq? @_url "url"))
-                expression: (string_expression
-                  (string_fragment) @string.special.path))
-              (binding
-                expression: (attrset_expression
-                  (binding_set
-                    binding: (binding
-                      attrpath: (attrpath
-                        attr: (identifier) @_url
-                        (#eq? @_url "url"))
-                      expression: (string_expression
-                        (string_fragment) @string.special.path)))))
-            ]))))))
+;; highlight uri in strings
+((string_fragment) @_uri
+  (#match?
+   @_uri
+   "^[a-zA-Z][a-zA-Z0-9\+\-\.]*:[a-zA-Z0-9%\/\?:@\&=\+\$,\-_\.\!\~\*\']+$" ; taken from upstream TS parser
+   )) @string.special.path
