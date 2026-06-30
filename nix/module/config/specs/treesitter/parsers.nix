d35c554702f22ff65f9d62f93a7f9f34df8895a8
Carman Fu <huivioemp1bs@posteo.com>
2026-06-30 13:17:17 +0800

    feat(mail): init treesitter support

--- nix/module/config/specs/treesitter/parsers.nix
+++ nix/module/config/specs/treesitter/parsers.nix
@@ -1,50 +1,56 @@
 { pkgs, ... }:
 ps:
-with ps; # pkgs.tree-sitter.builtGrammars.tree-sitter-*
 [
-  # atob # undefined variable
   pkgs.vimPlugins.nvim-treesitter.grammarPlugins.atob
-  bash
-  c
-  c_sharp
-  comment
-  css
-  dhall
-  diff
-  editorconfig
-  git_config
-  git_rebase
-  gitattributes
-  gitcommit
-  gitignore
-  go
-  groovy
-  haskell
-  haskell_persistent
-  html
-  idris
-  java
-  javascript
-  json
-  just
-  latex
-  lua
-  luadoc
-  markdown
-  nix
-  properties
-  python
-  query
-  regex
-  rust
-  scala
-  scheme
-  sql
-  toml
-  typst
-  vim
-  vimdoc
-  xml
-  yaml
-  zig
+  (pkgs.vimPlugins.nvim-treesitter.grammarToPlugin (
+    pkgs.tree-sitter-grammars.tree-sitter-mail
+  ))
 ]
+++ (
+  with ps; # pkgs.tree-sitter.builtGrammars.tree-sitter-*
+  [
+    # atob # undefined variable
+    bash
+    c
+    c_sharp
+    comment
+    css
+    dhall
+    diff
+    editorconfig
+    git_config
+    git_rebase
+    gitattributes
+    gitcommit
+    gitignore
+    go
+    groovy
+    haskell
+    haskell_persistent
+    html
+    idris
+    java
+    javascript
+    json
+    just
+    latex
+    lua
+    luadoc
+    markdown
+    nix
+    properties
+    python
+    query
+    regex
+    rust
+    scala
+    scheme
+    sql
+    toml
+    typst
+    vim
+    vimdoc
+    xml
+    yaml
+    zig
+  ])
