5e996ddd259afc20f50aa235906754c43b7f0ed7
Carman Fu <huivioemp1bs@posteo.com>
2026-07-04 21:11:59 +0800

    fix(hosts): disable ruby, perl, node

--- nix/module/config/hosts/default.nix
+++ nix/module/config/hosts/default.nix
@@ -2,5 +2,8 @@
 {
   config.hosts = {
     python3.withPackages = p: [ p.pexpect ];
+    ruby.nvim-host.enable = false;
+    perl.nvim-host.enable = false;
+    node.nvim-host.enable = false;
   };
 }
